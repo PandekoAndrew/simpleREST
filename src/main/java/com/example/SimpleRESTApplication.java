65906d06836e2769b3e64db93f655b2e105c322c
Andrei Pandzeka <Andrei.Pandzeka@itechart-group.com>
2018-09-27 14:10:57 +0300

    Create a project and add .gitignore

---
+++ src/main/java/com/example/SimpleRESTApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRESTApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRESTApplication.class, args);
+	}
+}
