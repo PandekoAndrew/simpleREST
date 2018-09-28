f6b8b0667da1c26c7160f294f208d858d0da8ee9
Andrei Pandzeka <Andrei.Pandzeka@itechart-group.com>
2018-09-28 13:37:15 +0300

    Add SimpleConfig and SimpleInterceptor

---
+++ src/main/java/com/example/config/SimpleConfig.java
@@ -0,0 +1,23 @@
+package com.example.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.filter.SimpleInterceptor;
+
+@Configuration
+public class SimpleConfig implements WebMvcConfigurer{
+	private SimpleInterceptor interceptor;
+	
+	@Autowired
+	public SimpleConfig(SimpleInterceptor interceptor) {
+		this.interceptor = interceptor;
+	}
+	
+	@Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(interceptor);
+    }
+}
